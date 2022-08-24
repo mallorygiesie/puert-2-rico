cd7ae8ddeed0333473188cbdc4533706040beaf2
Mallory Giesie <mallorygiesie@icloud.com>
2022-08-24 23:56:58 +0000

    graphed aaaaaahhhhh

--- question_2.R
+++ question_2.R
@@ -21,3 +21,10 @@ prm_clean <- prm %>% select(Sample_ID, Sample_Date, K, `NO3-N`) %>%
   filter(Year %in% c(2011,2012,2013,2014,2015,2016,2017,2018))
 
 #combining all of our tidy date into one data frame
+q1_allsites <- bind_rows(bq1_clean, bq2_clean, bq3_clean, prm_clean) %>% 
+  mutate(Month_Year = paste(lubridate::month(Sample_Date),lubridate::year(Sample_Date))) %>% 
+  mutate(Month_Year = lubridate:: my(Month_Year)) %>% 
+  group_by(Month_Year, Sample_ID) %>% summarise(mean = mean(K))
+
+ggplot(q1_allsites, aes(x = Month_Year , y = mean)) + geom_line(aes(color = Sample_ID))
+
